9b86b384ba866503dcf5e4cf262fb2c3cfcf9e19
islembriki <islembriki684@gmail.com>
2026-03-21 22:06:18 +0100

    first commit

---
+++ src/main/java/com/bibliotech/bibliotechproject/BiblioTechProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotech.bibliotechproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioTechProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiblioTechProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bibliotech/bibliotechproject/BiblioTechProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotech.bibliotechproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioTechProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
