1ad370f9a867a2497f136e9dcb5bc192e0f949ef
KhadraouiAmani <amani.khadraoui@insat.ucar.tn>
2026-03-22 21:57:53 +0100

    modifications

--- src/main/java/com/bibliotech/bibliotechproject/models/Author.java
+++ src/main/java/com/bibliotech/bibliotechproject/models/Author.java
@@ -4,11 +4,12 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data // Génère automatiquement les Getters et Setters
-@Document(collection = "authors") // Dit à MongoDB de créer une "table" authors
+@Data
+@Document(collection = "authors")
 public class Author {
-    @Id // Identifiant unique dans la base de données
+    @Id
     private String id;
     private String name;
     private String biography;
 }
+
